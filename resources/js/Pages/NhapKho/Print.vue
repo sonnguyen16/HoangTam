97636c261c1bc98968a3a5c7f1733b0d558dcc0b
giang_banbe <giang.bvu.it@gmail.com>
2024-01-26 09:11:47 +0700

    update

--- resources/js/Pages/NhapKho/Print.vue
+++ resources/js/Pages/NhapKho/Print.vue
@@ -40,10 +40,10 @@ function formatDateForTemplate(date) {
                        <!--<img :src="/uploads/ + don_vi.logo" class="w-[200px]" alt="logo" />--> 
                         <div class="text-start">
                             <h3 class="font-weight-bold mb-0" style="text-transform: uppercase;">{{ don_vi?.ten }}</h3>
-                            <p class="font-weight-bold mb-0">Địa chỉ: {{ don_vi?.dia_chi }}</p>
-                            <p class="font-weight-bold mb-0">Điện thoại: {{ don_vi?.dien_thoai }}</p>
-                            <p class="font-weight-bold mb-0" v-if="don_vi?.email">Email: {{ don_vi?.email }} </p>
-                            <p class="font-weight-bold mb-0" v-if="don_vi?.stk_1">STK: {{ don_vi?.stk_1 }} </p>
+                            <p class=" font-in" style="margin-bottom:0px">Địa chỉ: {{ don_vi?.dia_chi }}</p>
+                            <p class=" font-in" style="margin-bottom:0px">Điện thoại: {{ don_vi?.dien_thoai }}</p>
+                            <p class="font-in" style="margin-bottom:0px" v-if="don_vi?.email">Email: {{ don_vi?.email }} </p>
+                            <p class="font-in" style="margin-bottom:0px" v-if="don_vi?.stk_1">STK: {{ don_vi?.stk_1 }} </p>
                         </div>
                     </div>
 
@@ -58,47 +58,47 @@ function formatDateForTemplate(date) {
                             <div class="col-12">
                                 <table>
                                     <tr>
-                                    <td v-if="hoa_don.data.loai === 1" class="text-md">
+                                    <td v-if="hoa_don.data.loai === 1" class="text-md font-in">
                                         <b>Khách hàng:</b>
                                      </td>
-                                    <td v-if="hoa_don.data.loai === 0" class="text-md">
+                                    <td v-if="hoa_don.data.loai === 0" class="text-md font-in">
                                         <b>Nhà cung cấp:</b>
                                     </td>
-                                    <td width="70%">
+                                    <td width="70% " class="font-in">
                                         <span v-if="hoa_don.data.loai === 1"
-                                      class="text-md">
+                                      class="text-md font-in">
                                         {{ hoa_don.data.khach_hang?.ten }}
                                         </span>
                                         <span v-if="hoa_don.data.loai === 0"
-                                            class="text-md">
+                                            class="text-md font-in">
                                             {{ hoa_don.data.nha_cung_cap?.ten }}
                                         </span>
                                     </td>
-                                    <td class="text-md"><b>Số Phiếu:</b></td>
+                                    <td class="text-md font-in"><b>Số Phiếu:</b></td>
                                     <td>{{ hoa_don.data.ma }}</td>
                                     </tr>
                                     <tr>
-                                    <td class="text-md">
+                                    <td class="text-md font-in">
                                         <b>Địa chỉ:</b>
                                     </td>
                                     <td>
                                         <span v-if="hoa_don.data.loai === 1"
-                                      class="text-md">
+                                      class="text-md font-in">
                                         {{ hoa_don.data.khach_hang?.ten }}
                                         </span>
                                         <span v-if="hoa_don.data.loai === 0"
-                                            class="text-md">
+                                            class="text-md font-in">
                                             {{ hoa_don.data.nha_cung_cap?.ten }}
                                         </span>
                                     </td>
-                                    <td class="text-md"><b>Điện thoại:</b></td>
+                                    <td class="text-md font-in"><b>Điện thoại:</b></td>
                                     <td>
                                         <span v-if="hoa_don.data.loai === 1"
-                                       class="text-md">
+                                       class="text-md font-in">
                                     {{ hoa_don.data.khach_hang?.dien_thoai }}
                                 </span>
                                 <span v-if="hoa_don.data.loai === 0"
-                                      class="text-md">
+                                      class="text-md font-in" >
                                     {{ hoa_don.data.nha_cung_cap?.dien_thoai }}
                                 </span>
                                     </td>
@@ -106,35 +106,35 @@ function formatDateForTemplate(date) {
                                 </table>
                             </div>
                         </div>
-                        <table class="table table-bordered  table-responsive-md">
+                        <table class="table table-bordered"> <!-- table-bordered  table-responsive-md-->
                             <thead>
                             <tr>
-                                <th width="50">STT</th>
-                                <th>Tên sản phẩm</th>
-                                <th>Số lượng</th>
-                                <th>ĐVT</th>
-                                <th>Đơn giá</th>
-                                <th>Thành tiền</th>
+                                <!--<th style="width: 60px!important;" class="font-in" >STT</th> -->
+                                <th class="font-in">Tên sản phẩm</th>
+                                <th width="100" class="font-in">Số lượng</th>
+                                <th width="80" class="font-in">ĐVT</th>
+                                <th width="120" class="font-in">Đơn giá</th>
+                                <th width="120" class="font-in">Thành tiền</th>
                             </tr>
                             </thead>
                             <tbody>
                             <tr v-if="hoa_don.data.chi_tiet_hoa_don.length === 0">
-                                <td colspan="7" class="text-center">Không có dữ liệu</td>
+                                <td colspan="6" class="text-center font-in">Không có dữ liệu</td>
                             </tr>
 
                             <tr :key="cthd.id" v-else v-for="(cthd, index) in hoa_don.data.chi_tiet_hoa_don">
-                                <td class="ma">{{ index + 1 }}</td>
-                                <td class="ten">{{ cthd?.san_pham?.ten }}</td>
-                                <td class="quantity" >{{ cthd?.so_luong }}</td>
-                                <td class="quantity" >{{ cthd?.san_pham?.don_vi_tinh?.ten }}</td>
-                                <td class="money" >{{ cthd?.gia.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}</td>
-                                <td class="money" >{{ cthd?.thanh_tien.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}</td>
+                               <!-- <td class="ma font-in">{{ index + 1 }}</td> -->
+                                <td class="ten font-in">{{ index + 1 }}. {{ cthd?.san_pham?.ten }}</td>
+                                <td class="quantity font-in" >{{ cthd?.so_luong }}</td>
+                                <td class="quantity font-in" >{{ cthd?.san_pham?.don_vi_tinh?.ten }}</td>
+                                <td class="money font-in" >{{ cthd?.gia.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}</td>
+                                <td class="money font-in" >{{ cthd?.thanh_tien.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}</td>
                             </tr>
                             <tr>
-                                <td colspan="5"  class="text-md">
+                                <td colspan="4"  class="text-md font-in">
                                      <b>Tổng tiền</b>
                                 </td>
-                                <td class="money text-md">
+                                <td class="money text-md font-in">
                                     <b> {{ hoa_don.data.tong_tien.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}</b>
                                 </td>
                             </tr>
@@ -142,8 +142,8 @@ function formatDateForTemplate(date) {
                         </table>
 
                         <div class="row">
-                            <div class="col-12">
-                                <b>Bằng chữ:</b><i>{{ numberToWords(hoa_don.data.tong_tien) }} đồng</i>
+                            <div class="col-12 font-in">
+                                <b  class="font-in">Bằng chữ:</b><i>{{ numberToWords(hoa_don.data.tong_tien) }} đồng</i>
                             </div>
                         </div>
 
@@ -153,16 +153,16 @@ function formatDateForTemplate(date) {
 
                         <div class="d-flex justify-content-between w-100 mt-2"> <!--mt-5 -->
                             <div class="space-y-[60px] text-center">
-                                <p class="font-weight-bold text-md">Người giao hàng</p>
-                                <p>(Ký, họ tên)</p>
+                                <p class="font-weight-bold text-md font-in">Người giao hàng</p>
+                                <p  class="font-in">(Ký, họ tên)</p>
                             </div>
-                            <div class="space-y-[60px] text-center">
+                            <div class="space-y-[60px] text-center font-in">
                                 <p class="font-weight-bold text-md">Người nhận hàng</p>
-                                <p>(Ký, họ tên)</p>
+                                <p  class="font-in">(Ký, họ tên)</p>
                             </div>
-                            <div class="space-y-[60px] text-center">
-                                <p class="font-weight-bold text-md">Người lập phiếu</p>
-                                <p>(Ký, họ tên)</p>
+                            <div class="space-y-[60px] text-center font-in">
+                                <p class="font-weight-bold text-md font-in">Người lập phiếu</p>
+                                <p class="font-in">(Ký, họ tên)</p>
                             </div>
                         </div>
 
@@ -176,5 +176,11 @@ function formatDateForTemplate(date) {
 <style scoped>
 .bg-light-blue {
     background: rgba(227, 242, 253, 1);
+}
+.table-bordered td, .table-bordered th {
+    border: 1px solid black!important;
+}
+.font-in {
+    font-size: 14px;;
 }
 </style>

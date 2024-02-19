085f89d175867765d4ac4860d418ede697ab04c1
sonnguyen16 <sonnv2212@gmail.com>
2024-02-19 10:49:54 +0700

    update bao cao cong no

--- app/Http/Controllers/BaoCaoCongNoController.php
+++ app/Http/Controllers/BaoCaoCongNoController.php
@@ -49,7 +49,7 @@ class BaoCaoCongNoController extends Controller
             LEFT JOIN Chi c ON ncc.id = c.id
             LEFT JOIN TonDau td ON ncc.id = td.id
             LEFT JOIN TonCuoi tc ON ncc.id = tc.id
-            WHERE ncc.ten LIKE '%%' OR ncc.dien_thoai LIKE '%%' OR ncc.dia_chi LIKE '%%'
+            WHERE ncc.ten LIKE '%$search%' OR ncc.dien_thoai LIKE '%%' OR ncc.dia_chi LIKE '%$search%'
             AND ncc.DELETED_AT IS NULL
             GROUP BY ncc.id, ncc.ten, ncc.dien_thoai, ncc.dia_chi, c.chi, td.ton_dau, tc.ton_cuoi
             HAVING
@@ -73,30 +73,43 @@ class BaoCaoCongNoController extends Controller
 
         $query = "
             WITH Thu AS (
-            SELECT kh.id, COALESCE(SUM(ptc.so_tien), 0) AS thu
+                SELECT kh.id, COALESCE(SUM(ptc.so_tien), 0) AS thu
+                FROM khach_hang kh
+                LEFT JOIN phieu_thu_chi ptc ON kh.id = ptc.khach_hang_id
+                WHERE ptc.ngay BETWEEN ? AND ?
+                GROUP BY kh.id
+            ),
+            TonDau AS (
+                SELECT kh.id,
+                       kh.ton_dau + COALESCE(SUM(CASE WHEN hd.ngay < ? THEN cthd.so_luong ELSE 0 END), 0) AS ton_dau
+                FROM khach_hang kh
+                LEFT JOIN hoa_don hd ON kh.id = hd.khach_hang_id
+                LEFT JOIN chi_tiet_hoa_don cthd ON hd.id = cthd.hoa_don_id
+                GROUP BY kh.id, kh.ton_dau
+            ),
+            TonCuoi AS (
+                SELECT kh.id,
+                       kh.ton_dau + COALESCE(SUM(CASE WHEN hd.ngay <= ? THEN cthd.so_luong ELSE 0 END), 0) AS ton_cuoi
+                FROM khach_hang kh
+                LEFT JOIN hoa_don hd ON kh.id = hd.khach_hang_id
+                LEFT JOIN chi_tiet_hoa_don cthd ON hd.id = cthd.hoa_don_id
+                GROUP BY kh.id, kh.ton_dau
+            )
+            SELECT kh.id, kh.ten, kh.dien_thoai, kh.dia_chi, t.thu, td.ton_dau, tc.ton_cuoi
             FROM khach_hang kh
-            LEFT JOIN phieu_thu_chi ptc ON kh.id = ptc.khach_hang_id
-            WHERE ptc.ngay BETWEEN ? AND ?
-            GROUP BY kh.id
-        )
-        SELECT kh.id, kh.ten, kh.dien_thoai, kh.dia_chi, t.thu,
-            kh.ton_dau + COALESCE(SUM(CASE WHEN hd.ngay < ? THEN cthd.so_luong ELSE 0 END), 0) AS ton_dau,
-            kh.ton_dau + COALESCE(SUM(CASE WHEN hd.ngay <= ? THEN cthd.so_luong ELSE 0 END), 0) AS ton_cuoi
-        FROM khach_hang kh
-        JOIN users u ON kh.created_by = u.id AND u.don_vi_id = ?
-        LEFT JOIN hoa_don hd ON kh.id = hd.khach_hang_id
-        LEFT JOIN chi_tiet_hoa_don cthd ON hd.id = cthd.hoa_don_id
-        LEFT JOIN Thu t ON kh.id = t.id
-        LEFT JOIN phieu_thu_chi ptc ON kh.id = ptc.khach_hang_id
-        WHERE kh.ten LIKE '%$search%' OR kh.dien_thoai LIKE '%$search%' OR kh.dia_chi LIKE '%$search%'
-        AND kh.DELETED_AT IS NULL
-        GROUP BY kh.id, kh.ten, kh.dien_thoai, kh.dia_chi, t.thu, kh.ton_dau
-        HAVING
-            SUM(
-                IFNULL(t.thu, 0) +
-                IFNULL(SUM(CASE WHEN hd.ngay < ? THEN cthd.so_luong ELSE 0 END), 0) +
-                IFNULL(SUM(CASE WHEN hd.ngay <= ? THEN cthd.so_luong ELSE 0 END), 0)
-            ) != 0
+            JOIN users u ON kh.created_by = u.id AND u.don_vi_id = ?
+            LEFT JOIN Thu t ON kh.id = c.id
+            LEFT JOIN TonDau td ON kh.id = td.id
+            LEFT JOIN TonCuoi tc ON kh.id = tc.id
+            WHERE kh.ten LIKE '%$search%' OR kh.dien_thoai LIKE '%%' OR kh.dia_chi LIKE '%$search%'
+            AND kh.DELETED_AT IS NULL
+            GROUP BY kh.id, kh.ten, kh.dien_thoai, kh.dia_chi, t.thu, td.ton_dau, tc.ton_cuoi
+            HAVING
+                SUM(
+                    IFNULL(c.chi, 0) +
+                    IFNULL(td.ton_dau, 0) +
+                    IFNULL(tc.ton_cuoi, 0)
+                ) != 0
         ";
 
         $khach_hang_list = DB::select($query, [$ngayBatDau, $ngayKetThuc, $ngayBatDau, $ngayKetThuc, $don_vi_id]);
